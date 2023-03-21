520bb0d3fa53c00dd3393bef89f3752801a4a002
aliiahmadi <aliahmadi.github@gmail.com>
2023-03-21 16:22:34 +0330

    part 1

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     // DB::table("posts")->where("id", 10)->where("email", "ali@gmail.com")->get();
+
+//     // return view("main");
+// });
+
+
+ROute::get("/", "App\Http\Controllers\MainController@index");
